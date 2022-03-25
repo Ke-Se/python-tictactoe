89e1a602916d8c3c5cc68896897e0ce463c48846
Ke-Se <nstaubo@protonmail.com>
2022-03-25 12:54:06 +0100

    pygame makes a GUI now (nothing on it yet)

--- practice.py
+++ practice.py
@@ -1,53 +1,16 @@
-# Author: Nicolai Staubo
-# Program: Tic-Tac-Toe
+from turtle import width
+import pygame, sys
 
-# import the pygame module
-from curses import KEY_BACKSPACE, KEY_DOWN
-import pygame
-from pygame.locals import *
- 
-# Define our square object and call super to
-# give it all the properties and methods of pygame.sprite.Sprite
-# Define the class for our square objects
-class Square(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Square, self).__init__()
-         
-        # Define the dimension of the surface
-        # Here we are making squares of side 25px
-        self.surf = pygame.Surface((25, 25))
-         
-        # Define the color of the surface using RGB color coding.
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
- 
-# initialize pygame
 pygame.init()
- 
-# Define the dimensions of screen object
-screen = pygame.display.set_mode((800, 600))
 
-gameOn = True
+WIDTH = 600
+HEIGHT = 600
 
-# Our game loop
-while gameOn:
-    # for loop through the event queue
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Main game loop
+while True:
     for event in pygame.event.get():
-         
-        # Check for KEYDOWN event
-        if event.type == KEY_DOWN:
-             
-            # If the Backspace key has been pressed set
-            # running to false to exit the main loop
-            if event.key == KEY_BACKSPACE:
-                gameOn = False
-                 
-        # Check for QUIT event
-        elif event.type == quit:
-            gameOn = False
- 
-
-for y in range(height):
-    for x in range(width):
-        rect = pygame.Rect(x*block_size, y*block_size, block_size, block_size)
-        pygame.draw.rect(window, color, rect)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
--- tpractice.py
+++ tpractice.py
@@ -1,8 +1,42 @@
 import turtle
 
 vn = turtle.Screen()
+vn.title = "'Tic Tac Toe' Av Nicolai"
+vn.tracer(0)
 
-while True:
-    vn.update()
+turtle.speed(2)
+ 
+for i in range(4):
+    turtle.forward(300)
+    turtle.left(90)
 
-    
+turtle.penup()
+turtle.goto(0,100)
+turtle.pendown()
+ 
+turtle.forward(300)
+ 
+turtle.penup()
+turtle.goto(0,200)
+turtle.pendown()
+ 
+turtle.forward(300)
+ 
+turtle.penup()
+turtle.goto(100,0)
+turtle.pendown()
+ 
+turtle.left(90)
+turtle.forward(300)
+ 
+turtle.penup()
+turtle.goto(200,0)
+turtle.pendown()
+ 
+ 
+turtle.forward(300)
+
+turtle.done()
+
+        
+
